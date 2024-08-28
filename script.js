74bccaddab917a58e5c2275334c9ff115ee9110d
Noorunnabi <167161370+Noorunnabi-1@users.noreply.github.com>
2024-08-27 13:19:47 -1100

    Cynthia Ugwu website clone

---
+++ script.js
@@ -0,0 +1,6 @@
+// let div1 = document.querySelector("#palette-div1");
+
+// div1.addEventListener("mouseenter", function () {
+//     // console.log("lajfgfldja");
+//     div1.style.position = "absolute";
+// })
